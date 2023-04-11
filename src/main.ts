ba129b171cdd614028b73f04c391cb056c011892
Amirreza <95163272+shadow1375@users.noreply.github.com>
2023-04-11 22:10:59 +0330

    Ready for test

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function start() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   app.listen(process.env.PORT || 5000);
 }
 
