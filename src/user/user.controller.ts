5ad87b32584e843d3440bf5e15812210ed9dd8df
Amirreza <amirjob75@gmail.com>
2023-07-15 02:59:42 +0330

    File upload added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,21 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Delete,
+  FileTypeValidator,
   Get,
+  MaxFileSizeValidator,
   Param,
+  ParseFilePipe,
   ParseIntPipe,
   Post,
   Put,
+  Res,
   SetMetadata,
+  UploadedFile,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dtos/create-user.dto';
@@ -17,6 +24,9 @@ import { UpdateUserDto } from './dtos/update-user.dto';
 import { JwtGuardClass } from 'src/auth/guards/jwt-auth.guard';
 import { Roles } from './decorators/role.decorator';
 import { RoleGuard } from './guards/role.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import path = require('path');
 
 @Controller('api/user')
 export class UserController {
@@ -39,6 +49,8 @@ export class UserController {
   }
 
   @Post()
+  @UseGuards(JwtGuardClass, RoleGuard)
+  @Roles('ADMIN')
   async addUser(@Body() newUser: CreateUserDto) {
     const user = await this.userService.addUser(newUser);
 
@@ -56,9 +68,48 @@ export class UserController {
   }
 
   @Delete('/:id')
+  @UseGuards(JwtGuardClass, RoleGuard)
+  @Roles('ADMIN')
   async deleteUser(@Param('id', ParseIntPipe) id: number) {
     await this.userService.deleteUser(id);
 
     return 'User deleted';
+  }
+
+  @Post('/upload')
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          const fileName = path.parse(file.originalname).name;
+          const fileExt = path.parse(file.originalname).ext;
+
+          return cb(null, `${fileName}-${Date.now()}${fileExt}`);
+        },
+      }),
+      fileFilter: (req, file, cb) => {
+        const fileName = path.parse(file.originalname).name;
+        const fileExt = path.parse(file.originalname).ext;
+
+        if (fileExt !== '.jpg') {
+          return cb(new BadRequestException('File type should be jpg'), false);
+        }
+
+        return cb(null, true);
+      },
+    }),
+  )
+  uploadFile(
+    @Body() body: any,
+    @UploadedFile()
+    image: Express.Multer.File,
+  ) {
+    console.log(image);
+  }
+
+  @Get('/image/:imgName')
+  getProfilePhoto(@Param('imgName') imgName: string, @Res() res) {
+    return res.sendFile(imgName, { root: './uploads' });
   }
 }
