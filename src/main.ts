59b5a10472a16a0eaa5f85d6131f4dd253b5f11e
Amirreza <95163272+amir-96@users.noreply.github.com>
2023-07-15 23:35:29 +0330

    Cors added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions = {
+    origin: 'http://localhost:3000',
+  };
+
+  app.use(cors(corsOptions)); // Enable CORS only for http://localhost:3000
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(5000);
 }
+
 bootstrap();
